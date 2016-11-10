f178f687a3449f476f17c0cc4915456e92b7a861
Svetlana Linuxenko <linuxenko@yahoo.com>
2016-11-10 21:53:46 +0200

    fs tests

---
+++ tests/native/dummy-middleware.js
@@ -0,0 +1,32 @@
+var debouncy = require('debouncy');
+
+var sync = debouncy(function(data) {
+  return data;
+}, 1000, null);
+
+var Dummy = function() {
+
+};
+
+Dummy.prototype.fetch = function(json, callback) {
+  setTimeout(function() {
+    return callback(null, Buffer.from('hello world', 'utf8'));
+  }, 2000);
+};
+
+Dummy.prototype.sync = function(json, callback) {
+  sync(json);
+  if (callback) {
+    callback();
+  }
+};
+
+Dummy.prototype.post = function(json, buffer, callback) {
+  callback();
+};
+
+Dummy.prototype.del = function(json, callback) {
+  callback();
+};
+
+module.exports = Dummy;
---
+++ tests/native/index.js
@@ -0,0 +1,44 @@
+var fuse = require('fuse-bindings');
+var fs = require('fs');
+
+//var jsonData = require('../tests/fixture/flat');
+var MemFS = require('../../lib/memory-fs');
+var MemDrv = require('../../lib/memory-drv');
+var Dummy = require('./dummy-middleware');
+
+var dummyfs = new Dummy();
+
+var memfs = new MemFS([
+  {
+    path : '/hello.txt',
+    type : 'file',
+    size : 16
+  }
+], dummyfs);
+
+var memdrv = new MemDrv(memfs);
+
+var mountPath = process.platform !== 'win32' ? './mnt' : 'M:\\';
+
+
+var opts = memdrv.bindings();
+opts.options = ['nonempty'];
+
+fuse.mount(mountPath, opts, function (err) {
+  if (err) throw err;
+  console.log('filesystem mounted on ' + mountPath);
+});
+
+process.on('SIGINT', function () {
+
+  memfs.store.clean();
+  fs.rmdirSync(memfs.tmpDir);
+  fuse.unmount(mountPath, function (err) {
+    if (err) {
+      console.log('filesystem at ' + mountPath + ' not unmounted', err);
+    } else {
+      console.log('filesystem at ' + mountPath + ' unmounted');
+    }
+  });
+});
+
